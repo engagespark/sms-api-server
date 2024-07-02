aa21eeb2428a5ae3dbde71393c4d3f0ef071fd1d
Harmen <alicebob@lijzij.de>
2024-07-02 16:48:36 +0200

    need a docker image

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> {};
-{
-   default = pkgs.buildGoModule {
+let
+	smppd = pkgs.buildGoModule {
       name = "smppd";
       #buildInputs = [ gocache ];
       src = pkgs.lib.sourceByRegex ./. [
@@ -13,5 +13,26 @@ with import <nixpkgs> {};
       ];
       vendorHash = null; # uses ./vendor/
       doCheck = false;
+	};
+in
+{
+	default = smppd;
+	docker = pkgs.dockerTools.buildImage {
+    	name = "smppd";
+        tag = "latest";
+        copyToRoot = pkgs.buildEnv {
+          name = "smppd";
+          paths = [
+			pkgs.dockerTools.caCertificates
+    		pkgs.dockerTools.usrBinEnv
+    		pkgs.dockerTools.binSh
+			smppd
+         ];
+        };
+    config = {
+      Cmd = [ "/bin/smppd" ];
     };
+    compressor = "none"; # need this for `crane`
+  };
+
 }
