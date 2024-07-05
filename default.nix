6461da845d87edaf0b587336aff99aa25631a7b6
Harmen <alicebob@lijzij.de>
2024-07-05 11:22:10 +0200

    do exactly as Dockerfile does

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> {};
 let
-	smppd = pkgs.buildGoModule {
+    smppd = pkgs.buildGoModule {
       name = "smppd";
       #buildInputs = [ gocache ];
       src = pkgs.lib.sourceByRegex ./. [
@@ -10,29 +10,34 @@ let
         "main.go"
         "apiserver"
         "apiserver/.*"
+        "index.html"
       ];
       vendorHash = null; # uses ./vendor/
       doCheck = false;
-	};
+      postInstall = ''
+        mkdir -p $out/pub/
+        cp index.html $out/pub/
+      '';
+    };
 in
 {
-	default = smppd;
-	docker = pkgs.dockerTools.buildImage {
-    	name = "smppd";
+    default = smppd;
+    docker = pkgs.dockerTools.buildImage {
+        name = "smppd";
         tag = "latest";
         copyToRoot = pkgs.buildEnv {
           name = "smppd";
           paths = [
-			pkgs.dockerTools.caCertificates
-    		pkgs.dockerTools.usrBinEnv
-    		pkgs.dockerTools.binSh
-			smppd
+            pkgs.dockerTools.caCertificates
+            pkgs.dockerTools.usrBinEnv
+            pkgs.dockerTools.binSh
+            smppd
          ];
         };
-    config = {
-      Cmd = [ "/bin/sms-api-server" ];
+        config = {
+          Cmd = [ "/bin/sms-api-server" "-public" "/pub"];
+        };
+        compressor = "none"; # need this for `crane`
     };
-    compressor = "none"; # need this for `crane`
-  };
 
 }
